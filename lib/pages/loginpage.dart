ba2a40d7331e4c66066cbf8e85c9566a914bf1c8
Sagar k.c <sagarkc45172@gmail.com>
2023-02-14 21:14:25 +0545

    update

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -26,6 +26,40 @@ class LoginPage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(28))),
             ),
           ),
+          SizedBox(
+            height: 45,
+          ),
+          SizedBox(
+            height: 40,
+            width: 200,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff2B2D42),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              onPressed: () {},
+              child: Text("LOG IN"),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text("forget password"),
+          const Text("OR"),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff2B2D42),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              onPressed: () {},
+              child: Text("SIGN UP")),
+          Row(
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.facebook))
+            ],
+          )
         ]),
       ),
     );
