8b23a83ca0f8b0d34d3ca8aa6fd60ac525c069f4
Sagar k.c <sagarkc45172@gmail.com>
2023-02-15 19:46:50 +0545

    update

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_matchmaking_system/main.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,88 +7,105 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 100, left: 25, right: 25),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  label: const Text("Email/Phone"),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(28))),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SafeArea(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 100, left: 25, right: 25),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    label: const Text("Email/Phone"),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(28))),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 60, left: 25, right: 25),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  label: const Text("Password"),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(28))),
+            Padding(
+              padding: const EdgeInsets.only(top: 60, left: 25, right: 25),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    label: const Text("Password"),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(28))),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 45,
-          ),
-          SizedBox(
-            height: 40,
-            width: 200,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff2B2D42),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              onPressed: () {},
-              child: const Text("LOG IN"),
+            const SizedBox(
+              height: 45,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
-                Text(
-                  "forget password??",
-                  style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.red,
-                      fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            "OR",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            height: 40,
-            width: 100,
-            child: ElevatedButton(
+            SizedBox(
+              height: 40,
+              width: 200,
+              child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xff2B2D42),
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20))),
                 onPressed: () {},
-                child: const Text("SIGN UP")),
-          ),
-          Row(
-            children: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.facebook))
-            ],
-          )
-        ]),
+                child: const Text("LOG IN"),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Text(
+                    "forget password??",
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.red,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              "OR",
+              style: TextStyle(
+                  fontSize: 16, color: Colors.red, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 40,
+              width: 100,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff2B2D42),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                  onPressed: () {},
+                  child: const Text("SIGN UP")),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    iconSize: 50,
+                    onPressed: () {},
+                    icon: const Icon(Icons.facebook)),
+                IconButton(
+                    iconSize: 50,
+                    onPressed: () {},
+                    icon: const Icon(Icons.facebook)),
+                IconButton(
+                    iconSize: 50,
+                    onPressed: () {},
+                    icon: const Icon(Icons.facebook))
+              ],
+            )
+          ]),
+        ),
       ),
     );
   }
